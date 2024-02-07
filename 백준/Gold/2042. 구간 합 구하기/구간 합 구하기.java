5cccc2cadb49f839e5b0684e8a049fbfa54f7090
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-02-07 14:24:36 +0900

    [Gold I] Title: 구간 합 구하기, Time: 572 ms, Memory: 115928 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,96 +1,127 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine());
+	static long[] input;
+	static long[] tree;
+	
+	/**
+	 * 
+	 * tree 를 초기화
+	 * @param start		입력 데이타의 시작 index
+	 * @param end		입력 데이타의 끝 index
+	 * @param node		tree 의 중간 관리 값 또는 input 데이타를 저장할 index
+	 * 					start == end -> leaf node(tree[node]에 input[start] 데이타를 저장)
+	 * 					start != end -> 중간 node 이므로 관리값, start ~ end 까지의 관리 값이 저장됨
+	 * @return
+	 */
+	
+	public static long init(int start, int end, int node) {
+		
+		if(start == end) { // leaf node
+			return tree[node] = input[start];
+		}
+		
+		// 중간 node 이므로, 하위 두 노드의 관리할 값을 처리
+		int mid = (start + end) >> 1;		// 나누기 2보다 시프트 이동이 더 빠름
+		return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+	}
+	
+	/**
+	 * 구간 합
+	 * @param start		트리 관리 값의 시작 index
+	 * @param end		트리 관리 값의 끝 index
+	 * @param node		tree index
+	 * @param left		구간 합을 구할 시작 index
+	 * @param right		구간 합을 구할 끝 index
+	 * @return
+	 */
+	
+	public static long sum(int start, int end, int node, int left, int right) {
+		
+		// 범위 밖
+		if(left > end || right < start) {
+			return 0;
+		}
+		
+		// 구간 합을 구할 범위 내
+		if(left <= start && end <= right) {
+			return tree[node];
+		}
+		
+		// 탐색할 데이터가 start ~ end 사이에 있지만 딱 맞는 데이타가 아니므로 탐색
+		int mid = (start + end) >> 1;
+		return sum(start, mid, node*2, left, right) + sum(mid+1, end, node*2+1, left, right);
+		
+	}
+	
+	/**
+	 * update
+	 * @param start		트리 관리 값의 시작 index
+	 * @param end		트리 관리 값의 끝 index
+	 * @param node		tree index
+	 * @param index		update 할 input 데이터의 index
+	 * @param diff		update 할 데이터의 차이 값
+	 */
+	
+	public static void update(int start, int end, int node, int index, long diff) {
+		
+		if(index < start || index > end) {		// 범위 밖
+			return;
+		}
+		
+		// update 할 범위 내에 있는 node	
+		tree[node] += diff;			// 차이 값을 update
+		if(start == end) { 			// 단말 노드
+			input[index] = tree[node];
+			return;
+		}
+		
+		// 하위 노드 update
+		int mid = (start + end) >> 1;
+		update(start, mid, node*2, index, diff);		// 왼쪽 노드
+		update(mid+1, end, node*2+1, index, diff);		// 오른쪽 노드
+		
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		int N, M, K;
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		input = new long[N + 1];
+		tree = new long[N << 2];
+		
+		for(int i = 0; i < N; i++) {
+			input[i] = Long.parseLong(br.readLine());
+		}
+		
+		// 트리 생성
+		init(0, N-1, 1);
 
-        int n = Integer.parseInt(st.nextToken()); // 수의 개수
-        int m = Integer.parseInt(st.nextToken()); // 데이터 변경 개수
-        int k = Integer.parseInt(st.nextToken()); // 구간합 구하는 횟수
-
-        // 수 저장 배열
-        long[] arr = new long[n+1];
-        for(int i = 1; i <= n; i++){
-            arr[i] = Long.parseLong(br.readLine());
-        }
-
-        SegmentTree stree = new SegmentTree(n);
-        
-        stree.init(arr,1,1,n);
-
-        for(int i = 0; i < m+k; i++){
-            st = new StringTokenizer(br.readLine());
-
-            // 명령어
-            int cmd = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            // 원소의 범위는 2^63이므로 롱타입으로 받아야한다.
-            long b = Long.parseLong(st.nextToken());
-
-            // 데이터 변경 명령어
-            if(cmd == 1){
-                stree.update(1,1,n,a,b-arr[a]);
-                arr[a] = b;
-            // 구간합 명령어
-            }else{
-                bw.write(stree.sum(1,1,n,a,(int)b) +"\n");
-            }
-        }
-
-        bw.flush();
-        bw.close();
-    }
-
-    static class SegmentTree{
-        long tree[];
-        int treeSize;
-
-        public SegmentTree(int arrSize){
-            int h = (int) Math.ceil(Math.log(arrSize)/ Math.log(2));
-
-            this.treeSize = (int) Math.pow(2,h+1);
-            tree = new long[treeSize];
-        }
-
-        public long init(long[] arr, int node, int start,int end){
-            
-            if(start == end){
-                return tree[node] = arr[start];
-            }
-
-            return tree[node] =
-            init(arr,node*2,start,(start+ end)/2)
-            + init(arr,node*2+1,(start+end)/2+1,end);
-        }
-
-        public void update(int node, int start, int end, int idx, long diff){
-            if(idx < start || end < idx) return;
-
-            tree[node] += diff;
-
-            if(start != end){
-                update(node*2, start, (start+end)/2, idx, diff);
-                update(node*2+1, (start+end)/2+1, end, idx, diff);
-            }
-        }
-
-        public long sum(int node, int start, int end, int left, int right){
-            if(left > end || right < start){
-                return 0;
-            }
-
-            if(left <= start && end <= right){
-                return tree[node];
-            }
-
-            return sum(node*2, start, (start+end)/2, left, right)+
-                    sum(node*2+1, (start+end)/2+1, end, left, right);
-        }
-    }
+		for(int i = 0; i < M + K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			long c = Long.parseLong(st.nextToken());
+			
+			
+			if(a == 1) {
+				update(0, N-1, 1, b-1, c - input[b-1]);
+			}else if(a == 2) {
+				sb.append(sum(1, N, 1, b, (int)c)).append("\n");
+			}
+			
+		}
+		System.out.println(sb);
+	}
 }
