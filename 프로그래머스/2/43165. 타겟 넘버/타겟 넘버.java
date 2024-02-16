fd1e75c91dac5daf4399f90ab1c98fd4c2ba9739
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-02-17 01:58:55 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -3,11 +3,8 @@ class Solution {
     static int cnt = 0;
     
     public int solution(int[] numbers, int target) {
-        
-
         subset(0, numbers.length, 0, numbers, target);
         return cnt;
-        
     }
     
     static public void subset(int idx, int R, int sum, int[] numbers, int target){
@@ -16,7 +13,6 @@ class Solution {
             if(sum == target) cnt++;
             return;
         }
-        
         // 합함
         subset(idx + 1, R, sum + numbers[idx], numbers, target);
         // 뺌
