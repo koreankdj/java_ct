2212cd50b6c08759bfcd32b472789c8d338195b1
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-02-20 15:04:25 +0900

    [level 2] Title: 전화번호 목록, Time: 411.68 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        int cnt = phone_book.length;
+        Map<String, Integer> map = new HashMap<>();
+        
+        boolean answer = true;
+        for(int i = 0; i < cnt; i++) {
+        	map.put(phone_book[i], i);
+        }
+
+        for(int i = 0; i < cnt; i++) {
+        	for(int j = 0; j < phone_book[i].length(); j++) {
+        		if(map.containsKey(phone_book[i].substring(0, j))) {
+        			answer = false;
+        			return answer;
+        		}
+        	}
+        }
+
+        
+        return answer;
+    }
+}
