f676829084cf86f3c561c8b2212c11754f85b756
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-03-27 21:31:41 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,19 @@
+SET @hour = -1;
+
+SELECT(@hour := @hour + 1) AS HOUR,
+(SELECT COUNT(*)
+ FROM ANIMAL_OUTS
+ WHERE HOUR(DATETIME) = @hour) as 'COUNT'
+FROM ANIMAL_OUTS
+WHERE @hour < 23;
+
+
+/*
+SET @hour := -1;
+SELECT (@hour := @hour + 1) AS HOUR, 
+    (SELECT COUNT(*)
+     FROM ANIMAL_OUTS 
+     WHERE HOUR(DATETIME) = @hour) AS 'COUNT'
+FROM ANIMAL_OUTS
+WHERE @hour < 23
+*/
