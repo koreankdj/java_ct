927d9526b46175b999b337302065b9f73175bf8e
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-03-28 23:23:56 +0900

    [Silver II] Title: DFS와 BFS, Time: 360 ms, Memory: 24216 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,69 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.ArrayDeque;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	
 	static int N, M, V;
-	
-	static int[][] adjMatrix;
+	static int[][] map;
 	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		
+		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		V = Integer.parseInt(st.nextToken());
 		
-		adjMatrix = new int[N+1][N+1];
+		map = new int[N+1][N+1];
 		
+		
+		// 입력 받기 
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
 			int to = Integer.parseInt(st.nextToken());
-			adjMatrix[from][to] = 1;
-			adjMatrix[to][from] = 1;
+			
+			map[from][to] = 1;
+			map[to][from] = 1;
 		}
-		
-		visited = new boolean[N + 1];
+		visited = new boolean[N+1];
 		dfs(V);
 		System.out.println();
-		bfs();
-		
+		visited = new boolean[N+1];
+		bfs(V);
 	}
 	
-	public static void dfs(int cur) {
-
-		visited[cur] = true;
-		System.out.print(cur + " ");
+	public static void dfs(int k) {
+		visited[k] = true;
+		System.out.print(k + " ");
 		
 		for(int i = 1; i <= N; i++) {
-			if(!visited[i] && adjMatrix[cur][i] == 1) {
+			if(visited[i]) continue;
+			
+			if(map[k][i] == 1) {
 				dfs(i);
 			}
 		}
 	}
 	
-	public static void bfs() {
-		Queue<Integer> q = new LinkedList<>();
-		visited = new boolean[N+1];
-		
-		q.offer(V);
-		visited[V] = true;
+	public static void bfs(int start) {
+		ArrayDeque<Integer> q = new ArrayDeque<>();
+		q.add(start);
+		visited[start] = true;
 		
 		while(!q.isEmpty()) {
 			int cur = q.poll();
 			System.out.print(cur + " ");
-			
 			for(int i = 1; i <= N; i++) {
-				if(!visited[i] && adjMatrix[cur][i] == 1) {
+				if(visited[i]) continue;
+				
+				if(map[cur][i] == 1) {
 					q.offer(i);
 					visited[i] = true;
 				}
