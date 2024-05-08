4e75ddab576996883d68abc5a28b5eeffa255dd5
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-05-08 10:50:02 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,13 +1,20 @@
-SELECT M.MEMBER_NAME, R.REVIEW_TEXT, DATE_FORMAT(R.REVIEW_DATE, '%Y-%m-%d') as REVIEW_DATE
-FROM MEMBER_PROFILE as M
-JOIN REST_REVIEW as R on M.MEMBER_ID = R.MEMBER_ID
-WHERE M.MEMBER_ID in
-    (SELECT MEMBER_ID
-     FROM(
-        SELECT MEMBER_ID, DENSE_RANK() OVER (ORDER BY COUNT(*) DESC) as RNK
-        FROM REST_REVIEW 
-        GROUP BY MEMBER_ID
-        ) as SUB
-     WHERE RNK = 1
-    )
-ORDER BY 3, 2;
+-- 코드를 입력하세요
+
+with
+T1 as (
+    select member_id, rank() over(order by count(review_id) desc) as rnk
+    from REST_REVIEW
+    group by 1
+),
+T2 as (
+    select member_id
+    from T1
+    where rnk = 1
+)
+
+
+SELECT P.member_name, R.review_text, date_format(R.review_date, '%Y-%m-%d') as review_date
+from MEMBER_PROFILE as P
+join T2 as T on P.member_id = T.member_id
+join REST_REVIEW as R on T.member_id = R.member_id
+order by 3, 2;
