7e8412b556d31f5d7f4eca27602c0823e304738b
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-05-11 22:51:55 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,21 +1,29 @@
 -- 코드를 입력하세요
-
+/*
 with 
 T1 as (
-    select car_id
+    select car_id, count(history_id) as cnt
     from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-    where year(START_DATE) = 2022 and month(START_DATE) between 8 and 10
-    group by car_id
-    having count(history_id) >= 5
+    where year(start_date) = '2022' and month(start_date) between 8 and 10
+    group by 1
+    having cnt >= 5
+    order by 1
+),
+T2 as (
+    select month(start_date) as month, car_id, count(history_id) as records
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+    where car_id in (select car_id from T1)
+    group by 1, 2
 )
 
-select month(START_DATE) as month, car_id, count(history_id) as records
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-where car_id in (
-    select *
-    from T1
-) and year(start_date) = 2022 and month(start_date) between 8 and 10
-group by month, car_id
+SELECT * 
+from t2
 order by 1, 2 desc;
+*/
 
-
+SELECT month(start_date) as month, car_id, count(history_id) as records
+from CAR_RENTAL_COMPANY_RENTAL_HISTORY
+where start_date>="2022-08-01" and start_date<"2022-11-01" and car_id in (select car_id from CAR_RENTAL_COMPANY_RENTAL_HISTORY where start_date>="2022-08-01" and start_date<"2022-11-01" group by car_id having count(history_id)>4) 
+group by month, car_id
+having records >0
+order by month, car_id desc
