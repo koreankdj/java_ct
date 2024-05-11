e247089cba736b97811e169b12acc13166073bbb
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-05-11 22:24:49 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,32 +1,30 @@
-with
-T1 as ( -- 트럭인 데이터 뽑기
-    select *
+-- 코드를 입력하세요
+
+with 
+T1 as ( -- 트럭
+    select car_id, daily_fee
     from CAR_RENTAL_COMPANY_CAR
     where car_type = '트럭'
 ),
-T2 as ( -- 트럭의 기간 
-    select C.history_id, T.car_id, T.daily_fee, datediff(C.end_date, C.start_date)+1 as period, 
-    (case 
-     when datediff(end_date, start_date)+1 >= 90 then '90일 이상'
-     when datediff(end_date, start_date)+1 >= 30 then '30일 이상'
-     when datediff(end_date, start_date)+1 >= 7 then '7일 이상'
-     else 'NONE'
-    end) as duration_type
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY as C
-    join T1 as T on C.car_id = T.car_id
+T2 as (
+    select T.daily_fee, H.history_id, H.car_id, datediff(H.end_date, H.start_date) + 1 as period,
+    (case
+        when datediff(H.end_date, H.start_date) + 1 >= 90 then '90일 이상'
+        when datediff(H.end_date, H.start_date) + 1 >= 30 then '30일 이상'
+        when datediff(H.end_date, H.start_date) + 1 >= 7 then '7일 이상'
+        else 'NONE'
+    end) as duration_type 
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY as H
+    join T1 as T on H.car_id = T.car_id
+    order by 1
 ),
-T3 as ( -- 트럭의 할인율 뽑기
-    select duration_type, discount_rate
-    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+T3 as (
+    select car_type, duration_type, discount_rate
+    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
     where car_type = '트럭'
-),
-T4 as (
-    select A.history_id, 
-    round((A.period * A.daily_fee) * (100 - ifnull(B.discount_rate, 0)) / 100) as FEE
-    from T2 as A
-    left join T3 as B on A.duration_type = B.duration_type
 )
 
-select * 
-from T4
+select A.history_id, round((A.daily_fee * A.period) * (100 - ifnull(B.discount_rate, 0)) / 100) as fee
+from T2 as A
+left join T3 as B on A.duration_type = B.duration_type
 order by 2 desc, 1 desc;
