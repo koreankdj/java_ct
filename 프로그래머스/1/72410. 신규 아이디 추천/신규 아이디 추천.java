2c7ac5fd4cff6b69958cb91960bfb2acbba19233
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-05-12 01:23:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 10.97 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,132 +1,74 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
-    
-    /*
-        1. 아이디의 길이는 3자 이상 15자 이하
-        2. 아이디는 알파벳 소문자, 숫자, -, _, .
-        3. .는 처음과 끝에 사용할 수 없으며 연속으로 사용할 수 없다.
-        
-        
-        1. 모두 소문자로 치환
-        2. (2)에 해당하는 조건 수행
-        3. 마침표 2번 이상 ~ 하나로 치환
-        4. 마침표 처음이나 끝이면 제거
-        5. 빈 문자열이면 "a" 대입
-        6. 16자 이상이면, 첫 15를 제외한 나머지 문자들을 모두 제거.
-            제거 후, 마침표가 끝이라면 마침표 제거
-        7. 길이가 2자 이하라면 마지막 문자를 3이 될 때까지 반복
-    
-    */
-    
     public String solution(String new_id) {
-
+        String answer = "";
+        
         // 조건 1
-        String cond1 = new_id.toLowerCase();
-        //System.out.println("1: " + cond1);
+        String id = "";
+        id = new_id.toLowerCase();
         
-        // 조건 2 & 3
-        String cond2 = Cond2(cond1);
-        //System.out.println("2&3: " + cond2);
-
-        
-        // 조건 4 & 5
-        String cond4 = Cond4(cond2);
-        //System.out.println("4&5: " + cond4);
-
-        
-        // 조건 6
-        String cond6 = Cond6(cond4);
-        //System.out.println("6: " + cond6);
-        
-        
-        // 조건 7
-        String cond7 = Cond7(cond6);
-        //System.out.println("7: " + cond7);
-        
-        return cond7;
-    }
-    
-    public static String Cond2(String cond1){
-        
-        char[] arr = cond1.toCharArray();
-        int len = cond1.length();
-        
-        String cond2 = "";
-        boolean flag = false;
-        
-        for(int i = 0; i < len; i++){
+        // 조건2
+        String id2 = "";
+        for(int i = 0; i < id.length(); i++){
+            char c = id.charAt(i);
             
-            if(arr[i] - 'a' <= 25 && arr[i] - 'a' >= 0){
-                cond2 += arr[i];
-            }else if(arr[i] == '-' || arr[i] == '_' || arr[i] == '.'){
-                cond2 += arr[i];
-            }else if(Character.isDigit(arr[i])){
-                cond2 += arr[i];
-            }
-            
-            int k = cond2.length();
-            if(arr[i] == '.' && k > 1 && cond2.charAt(cond2.length()-2) == '.'){
-                cond2 = cond2.substring(0, cond2.length()-1);
+            if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '-' || c == '_' || c =='.'){
+                id2 += c;
             }
         }
         
-        return cond2;
-    }
-    
-    public static String Cond4(String cond3){
-        
-        if(cond3.length() == 0) return "a";
-        
-        String cond4 = "";
-        
-        // 앞이 . 이라면
-        if(cond3.charAt(0) == '.'){
-            cond4 = cond3.substring(1);
-        }else{
-            cond4 = cond3;
+        // 조건3
+        String id3 = id2.replace("..", ".");
+        while(id3.contains("..")){
+            id3 = id3.replace("..", ".");
         }
-        
-        if(cond4.length() == 0) return "a";
-        
-        // 뒤가 . 이라면
-        if(cond4.charAt(cond4.length()-1) == '.'){
-            cond4 = cond4.substring(0, cond4.length()-1);
-        }
-        
-        return cond4;
-    }
-    
-    public static String Cond6(String cond5){
-        int len = cond5.length();
-        String cond6 = "";
-        
-        if(len >= 16){
-            cond6 = cond5.substring(0, 15);
-            if(cond6.charAt(cond6.length()-1) == '.'){
-            cond6 = cond6.substring(0, 14);
+        //System.out.println("조건3 : " + id3);
+
+        // 조건4
+        String id4 = id3;
+        if(id4.length() > 0){
+            if(id4.charAt(0) == '.'){
+                id4 = id4.substring(1, id4.length());
             }
-        }else{
-            cond6 = cond5;
         }
-        
-        return cond6;
-    }
-    
-    public static String Cond7(String cond6){
-        int len = cond6.length();
-        String cond7 = "";
-        
-        if(len == 1){
-            cond7 = cond6 + cond6 + cond6;
-        }else if(len == 2){
-            cond7 = cond6 + cond6.charAt(1);
-        }else{
-            cond7 = cond6;
+        if(id4.length() > 0){
+            if(id4.charAt(id4.length()-1) == '.'){
+                id4 = id4.substring(0, id4.length()-1);
+            }
         }
+        //System.out.println("조건4 : " + id4);
         
-        return cond7;
+        // 조건5
+        String id5 = "";
+        if(id4.length() == 0){
+            id5 += 'a';
+        }else{
+            id5 = id4;
+        }
+        //System.out.println("조건5 : " + id5);
+        
+        // 조건6
+        String id6 = id5;
+        if(id6.length() >= 16){
+            id6 = id6.substring(0, 15);
+            if(id6.charAt(14) == '.'){
+                id6 = id6.substring(0, 14);
+            }
+        }
+        //System.out.println("조건6 : " + id6);
+        
+        // 조건7
+        String id7 = id6;
+        if(id7.length() == 1){
+            char c = id7.charAt(0);
+            id7 += c;
+            id7 += c;
+        }else if(id7.length() == 2){
+            char c = id7.charAt(1);
+            id7 += c;
+        }
+        //System.out.println("조건7 : " + id7);
+        
+        return id7;
     }
 
 }
