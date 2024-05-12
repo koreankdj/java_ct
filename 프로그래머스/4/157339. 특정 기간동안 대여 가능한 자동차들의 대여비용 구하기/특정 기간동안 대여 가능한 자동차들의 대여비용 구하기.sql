077d130b4d0583f371b9bcf45ca87dbabe706a11
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-05-12 10:00:47 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,40 +1,32 @@
 -- 코드를 입력하세요
-/*
 with 
-T1 as (     -- 대여 가능한 car_id 목록
-    select car_id
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-    where start_date >= '2022-12-01' or end_date <= '2022-10-31'
-    group by car_id
-),
-T2 as (
-    select car_id, car_type
+T1 as ( -- SUV or 세단
+    select car_id, car_type, (daily_fee * 30) as fee
     from CAR_RENTAL_COMPANY_CAR 
     where car_type in ('SUV', '세단')
+), 
+T2 as (
+    select *
+    from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+    where '2022-11-01' between start_date and end_date or '2022-11-30' between start_date and end_date
 ),
 T3 as (
-    select car_id, round((C.daily_fee * 30) * (100 - P.discount_rate)/100) as fee
-    from CAR_RENTAL_COMPANY_CAR as C
-    join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as P on C.car_type = P.car_type
-    where (C.daily_fee * 30) * (100 - P.discount_rate)/100 between 500000 and 2000000
-    group by 1
+    select car_id, car_type, fee
+    from T1
+    where car_id not in (select car_id from T2)
+),
+T4 as (
+    select car_type, discount_rate
+    from CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+    where car_type in ('세단', 'SUV') and duration_type = ('30일 이상')
+),
+T5 as (
+    select car_id, car_type, round(fee * (100 - discount_rate) / 100) as fee
+    from T3 
+    join T4 using (car_type)
+    where fee between 500000 and 2000000
 )
 
-select A.car_id, B.car_type, C.fee
-from T1 as A
-join T2 as B on A.car_id = B.car_id
-join T3 as C on A.car_id = C.car_id
+select * 
+from T5
 order by 3 desc, 2, 1 desc;
-*/
-SELECT C.CAR_ID, C.CAR_TYPE, ROUND(C.DAILY_FEE*30*(100-P.DISCOUNT_RATE)/100) AS FEE
-FROM CAR_RENTAL_COMPANY_CAR AS C
-JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY AS H ON C.CAR_ID=H.CAR_ID
-JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS P ON C.CAR_TYPE=P.CAR_TYPE
-WHERE C.CAR_ID NOT IN (
-    SELECT CAR_ID
-    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-    WHERE END_DATE > '2022-11-01' AND START_DATE < '2022-12-01'
-) AND P.DURATION_TYPE='30일 이상'
-GROUP BY C.CAR_ID
-HAVING C.CAR_TYPE IN ('세단', 'SUV') AND (FEE>=500000 AND FEE<2000000) 
-ORDER BY FEE DESC, CAR_TYPE, CAR_ID DESC
