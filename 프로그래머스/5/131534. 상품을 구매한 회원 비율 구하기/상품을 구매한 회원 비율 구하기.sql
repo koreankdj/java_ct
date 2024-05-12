7b199e0db455aad06e4922842e40d6878c206b41
Dongjin Kim <51356219+koreankdj@users.noreply.github.com>
2024-05-12 10:09:58 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -6,29 +6,15 @@ T1 as (
     from USER_INFO 
     where year(joined) = 2021
 ),
-T2 as(
-    select count(*) as total
-    from T1
-),
-T3 as (
-    select year(sales_date) as year, 
-    month(sales_date) as month, 
-    count(online_sale_id) as PUCHASED_USERS, 
-    round((count(online_sale_id) / (select * from T2)), 1) as PUCHASED_RATIO
+T2 as (
+    select *
     from ONLINE_SALE 
-    where user_id not in (select user_id from T1)
-    group by 1, 2
+    join T1 using (user_id)
 )
 
--- select * from T3 order by 1, 2;
-
-SELECT DATE_FORMAT(O.SALES_DATE, '%Y') AS YEAR,
-        DATE_FORMAT(O.SALES_DATE, '%m') AS MONTH,
-        COUNT(DISTINCT U.USER_ID) AS PUCHASED_USERS,
-        ROUND(COUNT(DISTINCT U.USER_ID)/(SELECT COUNT(*) FROM USER_INFO WHERE joined LIKE '2021%'), 1) AS PUCHASED_RATIO
-FROM USER_INFO U
-JOIN ONLINE_SALE O
-ON U.USER_ID = O.USER_ID
-WHERE U.JOINED LIKE '2021%'
-GROUP BY YEAR, MONTH
-ORDER BY YEAR, MONTH
+SELECT year(sales_date) as year, month(sales_date) as month, 
+count(distinct user_id) as PUCHASED_USERS,
+round((count(distinct user_id) / (select count(user_id) from T1)), 1) as PUCHASED_RATIO
+from t2
+group by 1, 2
+order by 1, 2;
